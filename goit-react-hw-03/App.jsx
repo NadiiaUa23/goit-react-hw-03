5dad6a724f9e64f3c986bbed33111a9958024ee5
NadiiaUa23 <hasiminka92@gmail.com>
2024-09-15 14:47:28 +0200

    Крок 2 - Пошук за ім'ям

--- goit-react-hw-03/App.jsx
+++ goit-react-hw-03/App.jsx
@@ -1,17 +1,35 @@
 import { useState } from "react";
 import contactsgrup from "./contacts.json";
 import ContactList from "./src/components/ContactList/ContactList";
-
+import SearchBox from "./src/components/SearchBox/SearchBox";
 
 
 
 const App = () => {
-    const [contacts, setPhone] = useState(contactsgrup)
+    const [contacts, setPhone] = useState(contactsgrup) // початковий масив контактів
+    const [filter, setFilter] = useState(''); // стан для фільтру
+
+//Ця функція оновлює стан filter кожного разу, коли користувач вводить текст у пошукове поле. event.target.value — це нове значення, яке ввів користувач.
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value);
+      };
+
+    const getFilteredContacts = () => {
+        //Спочатку фільтр перетворюється на нижній регістр (toLowerCase()), щоб зробити пошук нечутливим до регістр
+        const normalizedFilter = filter.toLowerCase();
+        return contacts.filter(contact =>
+          contact.name.toLowerCase().includes(normalizedFilter)
+        );
+      };
+
+      //Ця змінна зберігає результат фільтрації, тобто список контактів, що відповідають пошуковому запиту.
+      const visibleContacts = getFilteredContacts();
 
     return (
 <div>
     <h1 >Phonebook</h1>
-    <ContactList contacts={contacts} />
+    <SearchBox value={filter} onChange={handleFilterChange} />
+      <ContactList contacts={visibleContacts} />
 </div>
     );
 };
