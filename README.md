c9bff7379f2284886178828c5f3148fb4965608a
NadiiaUa23 <hasiminka92@gmail.com>
2024-09-17 20:22:26 +0200

    add ф-ція зберігання контакта в локал стозедж

--- README.md
+++
@@ -1,6 +0,0 @@
-# goit-react-hw-03
-
-npm create vite@latest goit-react-hw-03
-npm install formik
-npm install --save-dev prettier
-npm install -g vercel
--- goit-react-hw-03/App.jsx
+++ goit-react-hw-03/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import contactsgrup from "./contacts.json";
 import ContactList from "./src/components/ContactList/ContactList";
 import SearchBox from "./src/components/SearchBox/SearchBox";
 import ContactForm from "./src/components/ContactForm/ContactForm";
 
 const App = () => {
-    const [contacts, setContacts] = useState(contactsgrup) // початковий масив контактів
+    const [contacts, setContacts] = useState(() => {
+      const savedContacts = localStorage.getItem('contacts');
+      return savedContacts ? JSON.parse(savedContacts) : contactsgrup;
+  }); // початковий масив контактів
+    
     const [filter, setFilter] = useState(''); // стан для фільтру
+// для збереження контактів у localStorage після їх зміни
+    useEffect(() => {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]); 
+
 
     const addContact = (newContact) => {
         // Перевіряємо, чи контакт з таким іменем вже існує
@@ -15,6 +24,8 @@ const App = () => {
           return;
         }
         setContacts(prevContacts => [...prevContacts, newContact]);
+        
+        
     };
 
 //Ця функція оновлює стан filter кожного разу, коли користувач вводить текст у пошукове поле. event.target.value — це нове значення, яке ввів користувач.
@@ -22,6 +33,7 @@ const App = () => {
         setFilter(event.target.value);
       };
 
+        // Фільтруємо контакти
     const getFilteredContacts = () => {
         //Спочатку фільтр перетворюється на нижній регістр (toLowerCase()), щоб зробити пошук нечутливим до регістр
         const normalizedFilter = filter.toLowerCase();
